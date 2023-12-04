211887bd2a6768c7cced2ca093eeae8236a62080
apocalix <lololertrololer@gmail.com>
2023-12-04 00:06:03 -0600

    fix: ui changes
    visual fixes, frontend only

--- pages/index.js
+++ pages/index.js
@@ -67,20 +67,20 @@ function Home() {
     }
 
     return (
-        <main className="h-screen">
-            <header className="flex items-center justify-between gap-4 px-16 py-4 m-8 bg-red-400 rounded-lg">
-                <div className="flex items-center gap-4 text-2xl font-semibold text-white px-2">
-                    <FontAwesomeIcon icon={faCheckSquare} />
-                    <h1>Lector de CV's con IA 🤖</h1>
-                </div>
-                <FontAwesomeIcon
-                    icon={faIdBadge}
-                    className="flex items-center gap-4 text-4xl font-semibold text-white shadow-md rounded border-black hover:bg-black/10 hover:shadow-[3px_3px_0_1px_#671e1e] hover:translate-y-[-4px] hover:translate-x-[-4px] transition-all duration-100 hover:cursor-pointer"
-                />
-            </header>
-            <div className="flex items-center flex-col h-full w-full px-64 py-12 gap-8">
+        <main className="min-h-screen h-fit flex justify-center bg-red-50 max-lg:p-0 py-32">
+            <div className="flex items-center flex-col h-fit w-[750px] max-lg:border-0 border-2 border-black rounded-lg max-lg:p-4 max-lg:w-screen p-16 max-lg:gap-4 gap-8">
+                <header className="flex w-full items-center justify-between gap-4 max-lg:px-4 px-16 py-4 bg-red-400 rounded-lg">
+                    <div className="flex items-center max-lg:gap-2 gap-4 text-2xl font-semibold text-white">
+                        <FontAwesomeIcon className="max-lg:text-xl" icon={faCheckSquare} />
+                        <h1 className="max-lg:text-sm">Lector de CV's con IA 🤖</h1>
+                    </div>
+                    <FontAwesomeIcon
+                        icon={faIdBadge}
+                        className="flex items-center gap-4 max-lg:text-2xl text-4xl font-semibold text-white shadow-md rounded border-black hover:bg-black/10 hover:shadow-[3px_3px_0_1px_#671e1e] hover:translate-y-[-4px] hover:translate-x-[-4px] transition-all duration-100 hover:cursor-pointer"
+                    />
+                </header>
                 {/* <h1 className="text-5xl font-semibold text-red-400 pb-24">Lector de CV's con IA 🤖</h1> */}
-                <form className="flex w-full flex-col gap-4" onSubmit={handleSubmit}>
+                <form className="flex w-full flex-col max-lg:gap-2 gap-4" onSubmit={handleSubmit}>
                     {/* <p className="text-xl font-semibold">
                         Introduce los CV's que desees analizar en el siguiente recuadro:
                     </p> */}
@@ -88,50 +88,56 @@ function Home() {
                         value={curriculums}
                         onChange={(event) => setCurriculums(event.target.value)}
                         placeholder="Introduce aqui los CV's que desees analizar!"
-                        className="w-full min-h-[200px] max-h-[400px] border border-black rounded-md focus:outline-black p-2"
+                        className="w-full min-h-[200px] max-h-[400px] max-lg:text-sm border border-black rounded-md focus:outline-black p-2"
                     />
-                    <div className="flex w-full gap-4">
+                    <div className="flex w-full max-lg:gap-2 gap-4">
                         <button
                             type="submit"
                             disabled={curriculums.length < 1}
-                            className={` text-white w-full flex items-center justify-center gap-4 text-xl font-semibold py-2 px-4 rounded-lg border border-black ${
+                            className={` text-white w-full flex items-center justify-center gap-4 font-semibold py-2 px-4 rounded-lg border border-black ${
                                 curriculums.length > 0
                                     ? "bg-blue-400 hover:bg-blue-500 hover:shadow-[3px_3px_0_1px_#000000] hover:translate-y-[-4px] hover:translate-x-[-4px] transition-all duration-100 hover:cursor-pointer"
                                     : "bg-gray-400 cursor-not-allowed"
                             }`}
                         >
-                            <FontAwesomeIcon icon={faMagnifyingGlass} className="text-xl" />
-                            Analizar
+                            <FontAwesomeIcon icon={faMagnifyingGlass} className="text-xl max-lg:text-sm" />
+                            <p className="text-xl max-lg:text-sm">Analizar</p>
                         </button>
 
                         <button
                             onClick={handleClear}
-                            className="bg-red-400 text-white w-fit flex items-center justify-center gap-2 text-xl font-semibold py-2 px-4 rounded-lg border border-black hover:bg-red-500 hover:shadow-[3px_3px_0_1px_#000000] hover:translate-y-[-4px] hover:translate-x-[-4px] transition-all duration-100 hover:cursor-pointer"
+                            className="bg-red-400 text-white w-fit flex items-center justify-center gap-2 font-semibold py-2 px-4 rounded-lg border border-black hover:bg-red-500 hover:shadow-[3px_3px_0_1px_#000000] hover:translate-y-[-4px] hover:translate-x-[-4px] transition-all duration-100 hover:cursor-pointer"
                         >
-                            <FontAwesomeIcon icon={faBroom} className="text-xl" />
-                            Limpiar
+                            <FontAwesomeIcon icon={faBroom} className="text-xl max-lg:text-sm" />
+                            <p className="text-xl max-lg:text-sm">Limpiar</p>
                         </button>
                     </div>
                 </form>
-                <div className="flex w-full h-fit flex-col gap-4 bg-black/5 p-8 rounded-md border border-gray-300">
+                <div className="flex w-full h-fit flex-col gap-4 bg-black/5 max-lg:p-2 p-8 rounded-md border border-gray-300">
                     {feedback.length < 1 && !loading && (
                         <div className="flex flex-col items-center justify-center w-full h-full gap-4 py-8">
-                            <FontAwesomeIcon icon={faMagnifyingGlass} className="text-4xl" ref={iconRef} />
-                            <p className="text-xl font-semibold">Aqui aparecerán los resultados del analisis</p>
+                            <FontAwesomeIcon
+                                icon={faMagnifyingGlass}
+                                className="text-4xl max-lg:text-4xl"
+                                ref={iconRef}
+                            />
+                            <p className="text-xl max-lg:text-md font-semibold text-center">
+                                Aqui aparecerán los resultados del analisis
+                            </p>
                         </div>
                     )}
 
                     {loading && (
                         <div className="flex flex-col items-center justify-center w-full h-full gap-4 py-8 text-yellow-400">
                             <CircularProgress thickness={5} color="inherit" />
-                            <p className="text-xl font-semibold">Analizando...</p>
+                            <p className="text-xl font-semibold text-center">Analizando...</p>
                         </div>
                     )}
                     {(!loading && error) || (!loading && feedback.includes("RESPONSE_ERROR")) ? (
                         <div className="flex flex-col items-center justify-center w-full h-full gap-4 py-8 text-red-400">
                             <FontAwesomeIcon icon={faExclamationTriangle} className="text-4xl" />
                             <p className="text-xl font-semibold">Error:</p>
-                            <p className="text-xl font-semibold text-black">
+                            <p className="text-xl font-semibold text-black text-center">
                                 {error
                                     ? error
                                     : "Recuerda que unicamente puedes analizar curriculums con esta herramienta, intenta de nuevo!"}
